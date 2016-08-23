ecdaf1fa064a18ff632a76cf6d0e7f71897eb966
Ziv Zamechek <ziv921@yahoo.com>
2016-08-23 01:05:40 -0400

    Added username to new and edit form
    Added registration controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
