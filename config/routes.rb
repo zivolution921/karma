02c621b1ccdbb0d05eb1377602f0728406608fd3
Ziv Zamechek <ziv921@yahoo.com>
2016-08-24 00:04:36 -0400

    Added a user profile
    Changed the stylesheet to use jumbotron

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
   # devise_for :users
   devise_for :users, controllers: {registrations: 'registrations'}
   # You can have the root of your site routed with "root"
   root 'pages#home'
+
+   resources :users, only: [:show]
 end
