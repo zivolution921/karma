24677dbd12b6f0a4416a0235ba355d9b70c03097
Ziv Zamechek <ziv921@yahoo.com>
2016-08-24 20:36:38 -0400

    Added accept action for friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -16,6 +16,13 @@ class FriendshipsController < ApplicationController
     end
   end
 
+  def accept
+    @friendship.accept_friendship
+    respond_to do |format|
+      format.html {redirect_to users_path, notice: "Friendship Accepted"}
+    end
+  end
+
   private
 
   def set_user
