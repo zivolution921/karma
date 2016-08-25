3872f2c7321113efafcfa214bc1e12ade25d1a38
Ziv Zamechek <ziv921@yahoo.com>
2016-08-24 20:37:39 -0400

    modified nested routes for friendships

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   root 'pages#home'
 
    resources :users, only: [:show, :index]
-   resources :friendships, only: [:create, :destroy]
+   resources :friendships, only: [:create, :destroy, :accept] do 
+      member do 
+        put :accept
+      end
+   end
 end
